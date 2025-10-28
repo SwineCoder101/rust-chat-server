d3f11def7264d91f4e16cfd95d38ee0b8c0c39e7
Swinecoder101 <liamfaruq@gmail.com>
2025-10-28 19:14:25 +0000

    setup-topic-with-router

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 use anyhow::Result;
-use iroh::{Endpoint, SecretKey};
+use iroh::protocol::Router;
+use iroh::Endpoint;
+use iroh_gossip::{net::Gossip, proto::TopicId};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Generate a secret key. This is the source of
-    // identity for your endpoint. If you want to have
-    // the same identity each time you open the app,
-    // you would need to store and load it each time.
-    let secret_key = SecretKey::generate(&mut rand::rng());
-
-    // Create an endpoint.
-    // By default we turn on our n0 discovery services.
-    //  This allows you to
-    // dial by `EndpointId`, and allows you to be
-    // dialed by `EndpointId`.
-    let endpoint = Endpoint::builder()
-        // Pass in your secret key. If you don't pass
-        // in a secret key a new one will be generated
-        // for you each time.
-        .secret_key(secret_key)
-        // Bind the endpoint to the socket.
-        .bind()
-        .await?;
+    let endpoint = Endpoint::bind().await?;
 
     println!("> our endpoint id: {}", endpoint.id());
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn();
+
+    // Create a new topic.
+    let id = TopicId::from_bytes(rand::random());
+    let endpoint_ids = vec![];
+
+
+    let topic = gossip.subscribe(id, endpoint_ids).await?;
+
+
+    let (sender, _receiver) = topic.split();
+
+
+    sender.broadcast("sup".into()).await?;
+
+    router.shutdown().await?;
 
     Ok(())
 }
